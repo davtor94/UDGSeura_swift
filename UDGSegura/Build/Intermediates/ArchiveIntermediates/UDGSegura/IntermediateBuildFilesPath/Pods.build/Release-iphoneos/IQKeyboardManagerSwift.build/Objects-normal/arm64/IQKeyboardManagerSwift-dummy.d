84ead2218231cf2c48475ffc77444705069042ab
davtor94 <37162062+davtor94@users.noreply.github.com>
2018-03-13 14:41:50 -0600

    inital commit

---
+++ UDGSegura/Build/Intermediates/ArchiveIntermediates/UDGSegura/IntermediateBuildFilesPath/Pods.build/Release-iphoneos/IQKeyboardManagerSwift.build/Objects-normal/arm64/IQKeyboardManagerSwift-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/macmini1_cuceimobile/Dropbox/2017/UDGSegura/Pods/Target\ Support\ Files/IQKeyboardManagerSwift/IQKeyboardManagerSwift-dummy.m \
+  /Users/macmini1_cuceimobile/Dropbox/2017/UDGSegura/Build/Intermediates/ArchiveIntermediates/UDGSegura/IntermediateBuildFilesPath/Pods.build/Release-iphoneos/IQKeyboardManagerSwift.build/module.modulemap \
+  /Users/macmini1_cuceimobile/Dropbox/2017/UDGSegura/Pods/Target\ Support\ Files/IQKeyboardManagerSwift/IQKeyboardManagerSwift-prefix.pch
---
+++ UDGSegura/Build/Intermediates/ArchiveIntermediates/UDGSegura/IntermediateBuildFilesPath/Pods.build/Release-iphoneos/IQKeyboardManagerSwift.build/Objects-normal/arm64/IQKeyboardManagerSwift_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/macmini1_cuceimobile/Dropbox/2017/UDGSegura/Build/Intermediates/ArchiveIntermediates/UDGSegura/IntermediateBuildFilesPath/Pods.build/Release-iphoneos/IQKeyboardManagerSwift.build/DerivedSources/IQKeyboardManagerSwift_vers.c
